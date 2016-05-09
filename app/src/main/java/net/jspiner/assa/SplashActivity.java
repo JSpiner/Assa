733b6343a50ba6f70a9a492e7063fafb695aa06e
JSpiner <jspiner@naver.com>
2016-05-09 11:53:17 +0900

    init project

---
+++ app/src/main/java/net/jspiner/assa/SplashActivity.java
@@ -0,0 +1,17 @@
+package net.jspiner.assa;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright 2016 JSpiner. All rights reserved.
+ *
+ * @author JSpiner (jspiner@naver.com)
+ * @project Assa
+ * @since 2016. 5. 9.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+
+
+}
